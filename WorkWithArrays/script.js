95205279822b16239de60377cb80acc57aa0d652
ilinykh_ie <ilinykh-ie@mail.ru>
2020-11-27 17:12:40 +0700

    Исправление задачи WorkWithArrays.

--- WorkWithArrays/script.js
+++ WorkWithArrays/script.js
@@ -2,19 +2,19 @@
     var initialArray = [10, 8, 15, 2.2, -5, 345, 12, -6, 0];
     console.log(initialArray + " - исходный массив");
 
-    function getSortedArray(array) {
+    function sortDescending(array) {
         return array.sort(function (e1, e2) {
             return e2 - e1;
         });
     }
 
-    console.log(getSortedArray(initialArray) + " - массив, отсортированный по убыванию")
+    console.log(sortDescending(initialArray) + " - массив, отсортированный по убыванию");
 
     function getFirstFiveElements(array) {
         return array.slice(0, 5);
     }
 
-    console.log(getFirstFiveElements(initialArray) + " - первые 5 элементов")
+    console.log(getFirstFiveElements(initialArray) + " - первые 5 элементов");
 
     function getLastFiveElements(array) {
         return array.slice(initialArray.length - 5);
@@ -22,16 +22,15 @@
 
     console.log(getLastFiveElements(initialArray) + " - последние 5 элементов");
 
+    function isEven(element) {
+        return element % 2 === 0;
+    }
+
     function getEvenNumbersSum(array) {
-        var evenNumbersSum = 0;
-
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] % 2 === 0) {
-                evenNumbersSum += array[i];
-            }
-        }
-
-        return evenNumbersSum;
+        return array.filter(isEven)
+                    .reduce(function (accumulator, currentValue) {
+                        return accumulator + currentValue;
+                    }, 0);
     }
 
     console.log(getEvenNumbersSum(initialArray) + " - сумма четных элементов массива");
@@ -43,9 +42,10 @@
     }
 
     function getSquareEvenNumbersArray(array) {
-        return array.filter(x => x % 2 === 0)
-            .map(x => Math.pow(x, 2));
-
+        return array.filter(isEven)
+                    .map(function (element) {
+                        return Math.pow(element, 2)
+                    });
     }
 
     console.log(getSquareEvenNumbersArray(arrayFrom1To100) + " - список квадратов четных чисел");
